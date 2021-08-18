f327c9d942697a8ac0df1a96a8899a7b9335a792
Jonathan <johnjonathan948@gmail.com>
2021-08-17 22:17:24 -1200

    change

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ console.log('hello world');
 const btn = document.getElementById('cta')
 
 btn.addEventListener('click', function(){
-    document.querySelector('.name').innerHTML = 'Hello My Name Is (Jonathan John) see my Resume'
+    document.querySelector('.name').innerHTML = 'Hello My Name Is (Jonathan John)'
     // document.write('Hello My Name Is Jonathan John')
 })
